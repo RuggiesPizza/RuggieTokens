3b9aead86a77d3264e5faec2be5472e9364b486a
StinkyFi <stinkyzeroex@gmail.com>
2025-01-16 22:37:29 -0800

    Fixing pipeline

--- contracts/Sonic/RugTARDS2.sol
+++ contracts/Sonic/RugTARDS2.sol
@@ -5,11 +5,12 @@ pragma solidity ^0.8.24;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721Enumerable, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 // Local Imports
 import {RTARD} from "./Structs/RTARD.sol";
 
-contract RugTARDS2 is ERC721, ERC721Enumerable, Ownable {
+contract RugTARDS2 is ERC721Enumerable, Ownable {
     /// @dev Base Token URI
     string public baseURI;
     /// @dev Contract Beneficiary
@@ -20,8 +21,6 @@ contract RugTARDS2 is ERC721, ERC721Enumerable, Ownable {
     mapping(uint256 => RTARD) public rugged;
     /// @dev URI if rugged
     string public ruggedURI;
-    /// @dev track totalSupply
-    uint256 internal totalSupply;
 
     // Custom Errors
     error invalidAmount();
@@ -38,7 +37,6 @@ contract RugTARDS2 is ERC721, ERC721Enumerable, Ownable {
     constructor(address benef) ERC721("RugTARDS", "RTARD") Ownable(_msgSender())
     {
         beneficiary = benef;
-        totalSupply = 0;
     }
 
 
@@ -46,10 +44,9 @@ contract RugTARDS2 is ERC721, ERC721Enumerable, Ownable {
     /// @param holders list of holder addresses (must be in exact order from Fantom)
     function airdrop(address[] calldata holders) external onlyOwner {
         uint256 length = holders.length;
-        if(maxSupply < totalSupply + length) { revert MintedOut(); }
+        if(maxSupply < totalSupply() + length) { revert MintedOut(); }
         for (uint256 i; i < length; i++) {
-            totalSupply = totalSupply + 1;
-            _safeMint(holders[i], totalSupply);
+            _mint(holders[i], totalSupply());
         }
     }
 
@@ -61,8 +58,8 @@ contract RugTARDS2 is ERC721, ERC721Enumerable, Ownable {
         if(IERC20(token).balanceOf(address(this)) < amount) { revert invalidAmount(); }
 
         // Distribute to RTARD holders
-        uint256 perTokenShare = amount / totalSupply;
-        for (uint256 i = 1; i < totalSupply; i++) {
+        uint256 perTokenShare = amount / totalSupply();
+        for (uint256 i = 1; i < totalSupply(); i++) {
             address holder = ownerOf(i);
             if (holder != address(0) && perTokenShare > 0) {
                 IERC20(token).transfer(holder, perTokenShare);
@@ -73,7 +70,7 @@ contract RugTARDS2 is ERC721, ERC721Enumerable, Ownable {
     /// @dev Easter Egg
     /// @param tokenId The NFT you want to rug
     function rugpull(uint256 tokenId) external payable {
-        if(tokenId > totalSupply) { revert InvalidToken(); }
+        if(tokenId > totalSupply()) { revert InvalidToken(); }
         // 1 FTM to rug an NFT
         if(1 ether > msg.value) { revert InvalidPayment(); }
         rugged[tokenId].totalRugged++;
